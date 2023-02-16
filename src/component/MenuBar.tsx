bc7a86fea022a56cb6bcde93dd0f46b9b398d0c8
MAY-89 <hello.git.galic12@naver.com>
2023-02-17 01:12:06 +0900

    프론트엔드 세팅

--- src/component/MenuBar.tsx
+++ src/component/MenuBar.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./MenuBar.module.scss";
+import { Button, Col, Row, Space, Tooltip } from "antd";
+import { PlaySquareOutlined, SearchOutlined, SettingOutlined } from "@ant-design/icons";
+import Icon from "@ant-design/icons/lib/components/Icon";
 
 function MenuBar() {
   return (
-    <div className = "menuBar">
-      <div className='black-nav'>
-        <h4>상단 메뉴 1</h4>
-        <h4>상단 메뉴 1</h4>
-      </div>
+    <div>
+      <Row gutter={2} className={styles.menuBar}>
+        <Col xs={24} md={6} className={styles.menuBarItem}>
+          <Tooltip title="종목 설정">
+            <Button type="dashed" shape="default" icon={<SettingOutlined />} className={styles.button} />
+          </Tooltip>
+          <Tooltip title="실시간 크롤링">
+          <Button type="dashed" shape="default" icon={<PlaySquareOutlined />} className={styles.button}/>
+          </Tooltip>
+          <Tooltip title="search">
+            <Button type="dashed" shape="default" icon={<SearchOutlined />} className={styles.button}/>
+          </Tooltip>
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
 
-export default MenuBar
+export default MenuBar;
