bb47c893d023088a36f52818f4d68102ac84e1f3
MAY-89 <hello.git.galic12@naver.com>
2023-02-18 02:50:29 +0900

    코드 정리

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,43 +1,51 @@
 import Grid from "@toast-ui/react-grid";
 import React from "react";
-import { OptColumn, OptRow } from "tui-grid/types/options";
+import { OptColumn, OptHeader, OptRow } from "tui-grid/types/options";
+import Layout from "../component/Layout";
 import MenuBar from "../component/MenuBar";
+import styles from "./Main.module.scss";
+import "./grid.css"
 
 const columns: OptColumn[] = [
   {
-    header: 'Name',
-    name: 'name'
+    header: "Name",
+    name: "name",
+    align: "center",
   },
   {
-    header: 'Artist',
-    name: 'artist'
+    header: "Artist",
+    name: "artist",
+    align: "center",
   },
   {
-    header: 'Release',
-    name: 'release'
+    header: "Release",
+    name: "release",
+    align: "center",
   },
   {
-    header: 'Genre',
-    name: 'genre'
-  }
+    header: "Genre",
+    name: "genre",
+    align: "center",
+  },
 ];
 
 const data: OptRow[] = [
   {
-    name: 'Beautiful Lies',
-    artist: 'Birdy',
-    release: '2016.03.26',
-    genre: 'Pop'
-  }
-]
+    name: "Beautiful Lies",
+    artist: "Birdy",
+    release: "2016.03.26",
+    genre: "Pop",
+  },
+];
+
 
 function Main() {
   return (
     <div>
-      <MenuBar></MenuBar>
-      <Grid 
-      columns={columns}
-      data={data}></Grid>
+        <MenuBar />
+        <Grid className={styles.mainGrid} columns={columns} data={data}
+        >
+        </Grid>
     </div>
   );
 }
